14589c0040e00c86f219351ddc2c460975d93334
vinicius freire <vinicius.freirebispo@gmail.com>
2022-09-04 15:10:09 -0300

    Tabuada do 0 ao 10

---
+++ tabuada.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""Imprime a tabuada do 1 ao 10.
+Exemplo:
+Tabuada do 1
+1
+2
+3
+-----------
+Tabuada do 2
+2
+4
+6
+"""
+__version__ = '0.0.1'
+__author__ = 'Vinicius Freire'
+
+#numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+numeros = list(range(1,11))
+
+
+for numero in numeros:
+    print('Tabuada do:', numero)
+    for outro_numero in numeros:
+        print(numero * outro_numero)
+    print('--------------')
