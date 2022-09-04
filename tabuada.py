ffb5d6d38f67f0f9432697bf68534ff5bb12434d
vinicius freire <vinicius.freirebispo@gmail.com>
2022-09-04 15:47:15 -0300

    Formatando texto tabuada

--- tabuada.py
+++ tabuada.py
@@ -1,26 +1,27 @@
 #!/usr/bin/env python3
 """Imprime a tabuada do 1 ao 10.
 Exemplo:
-Tabuada do 1
-1
-2
-3
------------
-Tabuada do 2
-2
-4
-6
+--------- Tabuada do 1------
+1 x 1 = 1
+2 x 1 = 2
+3 x 1 = 3
+##############
+----------Tabuada do 2--------
+1 x 1 = 1
+2 x 2 = 4
+3 x 3 = 9
 """
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Vinicius Freire'
 
 #numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 numeros = list(range(1,11))
 
-
-for numero in numeros:
-    print('Tabuada do:', numero)
-    for outro_numero in numeros:
-        print(numero * outro_numero)
-    print('--------------')
+for n1 in numeros:
+    print("{:-^18}".format(f"Tabuada do {n1}"))
+    print()
+    for n2 in numeros:
+        resultado = n1 * n2
+        print("{:^18}".format(f"{n1} x {n2} = {resultado}"))
+    print("#" * 18)
