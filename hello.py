55e1e989d0bb0a041f52800b1e3aba21a286c52a
vinicius freire <vinicius.freirebispo@gmail.com>
2022-09-05 23:14:53 -0300

    adicionando input e outros modos de receber a variavel lang

--- hello.py
+++ hello.py
@@ -15,19 +15,45 @@ Execução:
      ou
      ./hello.py
 """
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 __author__ = "Vinicius Freire"
 
+from ast import arguments
 import os
+import sys
 
-current_language = os.getenv("LANG", "en_US")[:5]
+arguments = {
+    "lang": None,
+    "count": 1
+}
+for arg in sys.argv[1:]:
+    # TODO: Tratar ValueError
+    key, value = arg.split("=")
+    key = key.lstrip("-").strip()
+    value = value.strip()
+    if key not in arguments:
+        print(f"Invalid Option ´{key}´")
+        sys.exit()
+    arguments[key] = value
+
+current_language = arguments["lang"]
+if current_language is None:
+    current_language = os.getenv("LANG")
+    # TODO: Usar repetição
+    if current_language is None:
+        current_language = input("Choose a language:")
+
+current_language = current_language[:5]
 
 msg = {
     "en_US": "Hello, Word!",
     "pt_BR": "Olá, Mundo!",
     "it_IT": "Ciao, Mondo!",
     "es_ES": "Hola, Mundo!",
+    "fr_FR": "Bonjour, Monde!",
 
 }
 
-print(msg[current_language])
+print(
+    msg[current_language] * int(arguments["count"])
+)
